0c684d2b5f22696977ce73293597d2581a00212c
Shen Li <sshli@amazon.com>
2021-01-19 13:27:26 -0800

    Updates

---
+++ precache-manifest.dd5e93b53e55b633fa01954e1196a39c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17748fb07679e7c528200e124722a45c",
+    "url": "/h2-app/index.html"
+  },
+  {
+    "revision": "75e15510556842cd648b",
+    "url": "/h2-app/static/css/main.e1955600.chunk.css"
+  },
+  {
+    "revision": "9b12b382f57a916f3a98",
+    "url": "/h2-app/static/js/2.2db61dde.chunk.js"
+  },
+  {
+    "revision": "e0d700d349bbeae0845e0a2013bef690",
+    "url": "/h2-app/static/js/2.2db61dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e15510556842cd648b",
+    "url": "/h2-app/static/js/main.d51c57a7.chunk.js"
+  },
+  {
+    "revision": "dda6d6cfadf593c9462b",
+    "url": "/h2-app/static/js/runtime-main.d77a5d7b.js"
+  },
+  {
+    "revision": "10f8d0e10d8e2632fb847236be356205",
+    "url": "/h2-app/static/media/icon.10f8d0e1.png"
+  },
+  {
+    "revision": "77f5794e2eb49f7989b8f85e92cfa4e0",
+    "url": "/h2-app/static/media/no-img.77f5794e.png"
+  }
+]);
